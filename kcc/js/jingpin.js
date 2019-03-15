ea39cc23ac3bf5beb4fdf3437fd380635ad9c040
gzfgeh <gzfgeh@gmail.com>
2019-03-15 13:47:09 +0800

    update signature

--- kcc/js/jingpin.js
+++ kcc/js/jingpin.js
@@ -1,6 +1,5 @@
 var product_line_id=GetQueryString("product_line_id");
 
-
 //随机颜色
 function getRandomColor(index){ 
 	// return "#"+("00000"+((Math.random()*16777215+0.5)>>0).toString(16)).slice(-6); 
